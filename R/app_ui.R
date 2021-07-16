3555e2c3fe7727621237b821859ff404952e3765
Seb Dalgarno <seb.dalgarno@gmail.com>
2021-07-16 15:38:40 -0700

    add basic app ui

--- R/app_ui.R
+++ R/app_ui.R
@@ -1,34 +1,72 @@
 #' The application User-Interface
-#' 
-#' @param request Internal parameter for `{shiny}`. 
+#'
+#' @param request Internal parameter for `{shiny}`.
 #'     DO NOT REMOVE.
 #' @import shiny
 #' @noRd
 app_ui <- function(request) {
-  tagList(
-    # Leave this function for adding external resources
-    golem_add_external_resources(),
-    # Your application UI logic 
-    fluidPage(
-      h1("shinywsgcolr")
-    )
+
+  waiter::waiter_set_theme(color = "#749dbf")
+  title <- dashboardBrand(
+    title = "ffish lab",
+    color = "primary",
+    href = "https://www.ffishlab.ca/",
+    image = "https://raw.githubusercontent.com/poissonconsulting/logos/master/logos/poisson_hex.png"
+  )
+
+  dashboardPage(
+    freshTheme = mytheme,
+    dark = NULL,
+    scrollToTop = TRUE,
+    title = "Columbia River White Sturgeon",
+    header = dashboardHeader(
+      ... = div(h3("Columbia River White Sturgeon"), style = "vertical-align: baseline;"),
+      title = title
+    ),
+    sidebar = dashboardSidebar(
+      skin = "light",
+      elevation = 3,
+      id = "sidebarmenu",
+      sidebarHeader("Acoustic Telemetry"),
+      menuItem(
+        "Deployment",
+        tabName = "deployment"
+      ),
+      menuItem(
+        "Detection Path",
+        tabName = "detection_path"
+      ),
+      menuItem(
+        "Detection Ratios",
+        tabName = "detection_ratio"
+      )
+    ),
+    controlbar = dashboardControlbar(),
+    footer = dashboardFooter(
+      left = a(
+        href = "https://www.ffishlab.ca/",
+        target = "_blank", "Freshwater Fish Ecology Lab"
+      ),
+      right = "2021"
+    ),
+    body = dashboardBody()
   )
 }
 
 #' Add external Resources to the Application
-#' 
-#' This function is internally used to add external 
-#' resources inside the Shiny application. 
-#' 
+#'
+#' This function is internally used to add external
+#' resources inside the Shiny application.
+#'
 #' @import shiny
 #' @importFrom golem add_resource_path activate_js favicon bundle_resources
 #' @noRd
 golem_add_external_resources <- function(){
-  
+
   add_resource_path(
     'www', app_sys('app/www')
   )
- 
+
   tags$head(
     favicon(),
     bundle_resources(
@@ -36,7 +74,7 @@ golem_add_external_resources <- function(){
       app_title = 'shinywsgcolr'
     )
     # Add here other external resources
-    # for example, you can add shinyalert::useShinyalert() 
+    # for example, you can add shinyalert::useShinyalert()
   )
 }
 
---
+++ R/utils.R
@@ -0,0 +1,9 @@
+mytheme <- fresh::create_theme(
+  fresh::bs4dash_status(
+    primary = "#749dbf"
+  ),
+
+  fresh::bs4dash_color(
+    blue = "#749dbf",
+  )
+)
